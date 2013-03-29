47cae0d3c05020d57dc6069c5e3e711c77e03de2
Dmitry Brant <brant.dmitry@gmail.com>
2013-03-28 22:08:45 -0400

    Initial commit, importing from svn...

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace MpoViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.cbMode = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.lblImage = new System.Windows.Forms.ToolStripLabel();
+            this.cbImage = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnCycle = new System.Windows.Forms.ToolStripButton();
+            this.btnSlower = new System.Windows.Forms.ToolStripButton();
+            this.btnFaster = new System.Windows.Forms.ToolStripButton();
+            this.btnAbout = new System.Windows.Forms.ToolStripButton();
+            this.timerCycle = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSave});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(144, 26);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Image = ((System.Drawing.Image)(resources.GetObject("mnuSave.Image")));
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(143, 22);
+            this.mnuSave.Text = "Save image...";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(664, 383);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(210)))), ((int)(((byte)(255)))));
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpen,
+            this.toolStripSeparator1,
+            this.toolStripLabel2,
+            this.cbMode,
+            this.toolStripSeparator3,
+            this.lblImage,
+            this.cbImage,
+            this.toolStripSeparator2,
+            this.btnCycle,
+            this.btnSlower,
+            this.btnFaster,
+            this.btnAbout});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(688, 30);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.TabStop = true;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.AutoToolTip = false;
+            this.btnOpen.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
+            this.btnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(66, 27);
+            this.btnOpen.Text = "Open...";
+            this.btnOpen.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 30);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(41, 27);
+            this.toolStripLabel2.Text = "Mode:";
+            // 
+            // cbMode
+            // 
+            this.cbMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMode.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cbMode.Items.AddRange(new object[] {
+            "Single",
+            "Stereo (cross-eye)",
+            "Stereo (relaxed-eye)"});
+            this.cbMode.Name = "cbMode";
+            this.cbMode.Size = new System.Drawing.Size(130, 30);
+            this.cbMode.SelectedIndexChanged += new System.EventHandler(this.cbMode_SelectedIndexChanged);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 30);
+            // 
+            // lblImage
+            // 
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(43, 27);
+            this.lblImage.Text = "Image:";
+            // 
+            // cbImage
+            // 
+            this.cbImage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbImage.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.cbImage.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.cbImage.Name = "cbImage";
+            this.cbImage.Size = new System.Drawing.Size(75, 30);
+            this.cbImage.SelectedIndexChanged += new System.EventHandler(this.cbImage_SelectedIndexChanged);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 30);
+            // 
+            // btnCycle
+            // 
+            this.btnCycle.AutoToolTip = false;
+            this.btnCycle.CheckOnClick = true;
+            this.btnCycle.Image = ((System.Drawing.Image)(resources.GetObject("btnCycle.Image")));
+            this.btnCycle.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCycle.Name = "btnCycle";
+            this.btnCycle.Size = new System.Drawing.Size(56, 27);
+            this.btnCycle.Text = "Cycle";
+            this.btnCycle.Click += new System.EventHandler(this.btnCycle_Click);
+            // 
+            // btnSlower
+            // 
+            this.btnSlower.AutoToolTip = false;
+            this.btnSlower.Image = ((System.Drawing.Image)(resources.GetObject("btnSlower.Image")));
+            this.btnSlower.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSlower.Name = "btnSlower";
+            this.btnSlower.Size = new System.Drawing.Size(62, 27);
+            this.btnSlower.Text = "Slower";
+            this.btnSlower.Click += new System.EventHandler(this.btnSlower_Click);
+            // 
+            // btnFaster
+            // 
+            this.btnFaster.AutoToolTip = false;
+            this.btnFaster.Image = ((System.Drawing.Image)(resources.GetObject("btnFaster.Image")));
+            this.btnFaster.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFaster.Name = "btnFaster";
+            this.btnFaster.Size = new System.Drawing.Size(58, 27);
+            this.btnFaster.Text = "Faster";
+            this.btnFaster.Click += new System.EventHandler(this.btnFaster_Click);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnAbout.AutoToolTip = false;
+            this.btnAbout.Image = ((System.Drawing.Image)(resources.GetObject("btnAbout.Image")));
+            this.btnAbout.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(69, 27);
+            this.btnAbout.Text = "About...";
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // timerCycle
+            // 
+            this.timerCycle.Interval = 250;
+            this.timerCycle.Tick += new System.EventHandler(this.timerCycle_Tick);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 429);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnOpen;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel lblImage;
+        private System.Windows.Forms.ToolStripComboBox cbImage;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnCycle;
+        private System.Windows.Forms.ToolStripButton btnSlower;
+        private System.Windows.Forms.ToolStripButton btnFaster;
+        private System.Windows.Forms.Timer timerCycle;
+        private System.Windows.Forms.ToolStripButton btnAbout;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripComboBox cbMode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+/*
+    Copyright (C) 2011-2013 Dmitry Brant <me@dmitrybrant.com>
+  
+    This software is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+  
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write the Free Software Foundation, Inc., 51
+    Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ */
+namespace MpoViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Functions.FixDialogFont(this);
+            this.Text = Application.ProductName;
+
+            images = new List<Image>();
+
+            cbMode.SelectedIndex = 0;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var openDlg = new OpenFileDialog();
+            openDlg.DefaultExt = ".mpo";
+            openDlg.CheckFileExists = true;
+            openDlg.Title = "Open MPO file...";
+            openDlg.Filter = "MPO Files (*.mpo)|*.mpo|All Files (*.*)|*.*";
+            openDlg.FilterIndex = 1;
+            if (openDlg.ShowDialog() == DialogResult.Cancel) return;
+            OpenMPO(openDlg.FileName);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                e.Effect = DragDropEffects.All;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 0) return;
+            OpenMPO(files[0]);
+        }
+
+
+        private List<Image> images;
+        private Image stereoImage = null;
+        private string FileName = "";
+
+
+        private void OpenMPO(string fileName)
+        {
+            FileName = fileName;
+            images.Clear();
+            cbImage.Items.Clear();
+
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                byte[] tempBytes = new byte[100];
+                using (var f = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    tempBytes = new byte[f.Length];
+                    f.Read(tempBytes, 0, (int)f.Length);
+                }
+
+                List<int> imageOffsets = new List<int>();
+                int offset = 0, tempOffset = 0;
+                byte[] keyBytes = { 0xFF, 0xD8, 0xFF, 0xE1 };
+                byte[] keyBytes2 = { 0xFF, 0xD8, 0xFF, 0xE0 };
+
+                while (true)
+                {
+                    tempOffset = Functions.SearchBytes(tempBytes, keyBytes, offset, tempBytes.Length);
+                    if (tempOffset == -1)
+                        tempOffset = Functions.SearchBytes(tempBytes, keyBytes2, offset, tempBytes.Length);
+                    if (tempOffset == -1) break;
+                    offset = tempOffset;
+                    imageOffsets.Add(offset);
+                    offset += 4;
+                }
+
+                for (int i = 0; i < imageOffsets.Count; i++)
+                {
+                    int length;
+                    if (i < (imageOffsets.Count - 1))
+                        length = imageOffsets[i + 1] - imageOffsets[i];
+                    else
+                        length = tempBytes.Length - imageOffsets[i];
+
+                    MemoryStream stream = new MemoryStream(tempBytes, imageOffsets[i], length);
+                    images.Add(new Bitmap(stream));
+                    cbImage.Items.Add((i + 1).ToString());
+                }
+
+                if (images.Count == 0)
+                {
+                    pictureBox.Image = null;
+                    MessageBox.Show(this, "This does not appear to be a valid MPO file.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    cbImage.SelectedIndex = 0;
+                    cbMode_SelectedIndexChanged(null, null);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            if (images.Count == 0) return;
+            try
+            {
+                var saveDlg = new SaveFileDialog();
+                saveDlg.DefaultExt = ".jpg";
+                saveDlg.OverwritePrompt = true;
+                saveDlg.Title = "Save file...";
+                saveDlg.Filter = "JPG Files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+                saveDlg.FilterIndex = 1;
+                saveDlg.InitialDirectory = Path.GetDirectoryName(FileName);
+                saveDlg.FileName = Path.GetFileNameWithoutExtension(FileName) + "_" + (cbImage.SelectedIndex + 1).ToString() + ".jpg";
+                if (saveDlg.ShowDialog() == DialogResult.Cancel) return;
+
+                images[cbImage.SelectedIndex].Save(saveDlg.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void cbImage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox.Image = images[cbImage.SelectedIndex];
+            }
+            catch { }
+        }
+
+        private void timerCycle_Tick(object sender, EventArgs e)
+        {
+            if (cbImage.Items.Count == 0) return;
+            if (cbMode.SelectedIndex == 1) return;
+
+            int i = cbImage.SelectedIndex + 1;
+            if (i >= cbImage.Items.Count) i = 0;
+            cbImage.SelectedIndex = i;
+        }
+
+        private void btnSlower_Click(object sender, EventArgs e)
+        {
+            timerCycle.Interval += 25;
+        }
+
+        private void btnFaster_Click(object sender, EventArgs e)
+        {
+            if (timerCycle.Interval > 25)
+                timerCycle.Interval -= 25;
+        }
+
+        private void btnCycle_Click(object sender, EventArgs e)
+        {
+            timerCycle.Enabled = !timerCycle.Enabled;
+        }
+
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, Application.ProductName + "\nCopyright © 2011 by Dmitry Brant\n\nhttp://dmitrybrant.com", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void cbMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblImage.Enabled = cbImage.Enabled = btnCycle.Enabled = btnFaster.Enabled = btnSlower.Enabled = (cbMode.SelectedIndex == 0);
+
+
+            if (cbMode.SelectedIndex == 0)
+            {
+                cbImage_SelectedIndexChanged(null, null);
+            }
+            else if (cbMode.SelectedIndex == 1)
+            {
+                if (images.Count == 0) return;
+
+                //rebuild the stereo image...
+                int totalWidth = 0, totalHeight = 0;
+
+                foreach (var img in images)
+                {
+                    totalWidth += img.Width;
+                    if (img.Height > totalHeight)
+                        totalHeight = img.Height;
+                }
+
+                stereoImage = new Bitmap(totalWidth, totalHeight);
+                var g = Graphics.FromImage(stereoImage);
+
+                int curX = 0;
+                for (int i = images.Count - 1; i >= 0; i--)
+                {
+                    g.DrawImage(images[i], new Rectangle(curX, 0, images[i].Width, totalHeight), new Rectangle(0, 0, images[i].Width, images[i].Height), GraphicsUnit.Pixel);
+                    curX += images[i].Width;
+                }
+
+                pictureBox.Image = stereoImage;
+            }
+            else if (cbMode.SelectedIndex == 2)
+            {
+                if (images.Count == 0) return;
+
+                //rebuild the stereo image...
+                int totalWidth = 0, totalHeight = 0;
+
+                foreach (var img in images)
+                {
+                    totalWidth += img.Width;
+                    if (img.Height > totalHeight)
+                        totalHeight = img.Height;
+                }
+
+                stereoImage = new Bitmap(totalWidth, totalHeight);
+                var g = Graphics.FromImage(stereoImage);
+
+                int curX = 0;
+                for (int i = 0; i < images.Count; i++)
+                {
+                    g.DrawImage(images[i], new Rectangle(curX, 0, images[i].Width, totalHeight), new Rectangle(0, 0, images[i].Width, images[i].Height), GraphicsUnit.Pixel);
+                    curX += images[i].Width;
+                }
+
+                pictureBox.Image = stereoImage;
+            }
+        }
+
+    }
+
+
+
+    public class Functions
+    {
+
+        /// <summary>
+        /// Sets the font of a given control, and all child controls, to
+        /// the current system font, while preserving font styles.
+        /// </summary>
+        /// <param name="c0">Control whose font will be set.</param>
+        public static void FixDialogFont(Control c0)
+        {
+            Font old = c0.Font;
+            c0.Font = new Font(SystemFonts.MessageBoxFont.FontFamily.Name, old.Size, old.Style);
+            if (c0.Controls.Count > 0)
+                foreach (Control c in c0.Controls)
+                    FixDialogFont(c);
+        }
+
+
+        /// <summary>
+        /// Search an array of bytes for a byte pattern specified in another array.
+        /// </summary>
+        /// <param name="bytesToSearch">Array of bytes to search</param>
+        /// <param name="matchBytes">Byte pattern to search for</param>
+        /// <param name="startIndex">Starting index within the first array to start searching</param>
+        /// <param name="count">Number of bytes in the first array to search</param>
+        /// <returns>Zero-based index of the beginning of the byte pattern found in 
+        /// the byte array, or -1 if not found.</returns>
+        public static int SearchBytes(byte[] bytesToSearch, byte[] matchBytes, int startIndex, int count)
+        {
+            int ret = -1, max = count - matchBytes.Length + 1;
+            bool found;
+            for (int i = startIndex; i < max; i++)
+            {
+                found = true;
+                for (int j = 0; j < matchBytes.Length; j++)
+                {
+                    if (bytesToSearch[i + j] != matchBytes[j]) { found = false; break; }
+                }
+                if (found) { ret = i; break; }
+            }
+            return ret;
+        }
+        public static int SearchBytes(byte[] bytesToSearch, string matchStr, int startIndex, int count)
+        {
+            byte[] matchBytes = Encoding.ASCII.GetBytes(matchStr);
+            return SearchBytes(bytesToSearch, matchBytes, startIndex, count);
+        }
+
+        /// <summary>
+        /// Compare two arrays of bytes.
+        /// </summary>
+        /// <param name="array1">First array to compare.</param>
+        /// <param name="start1">Starting index in the first array to begin comparing.</param>
+        /// <param name="array2">Second array to compare.</param>
+        /// <param name="start2">Starting index in the second array to begin comparing.</param>
+        /// <param name="count">Number of bytes to compare.</param>
+        /// <returns>True if the bytes are identical, false otherwise.</returns>
+        public static bool MemCmp(byte[] array1, int start1, byte[] array2, int start2, int count)
+        {
+            bool ret = true;
+            for (int i = 0; i < count; i++)
+            {
+                if (array1[start1] != array2[start2]) { ret = false; break; }
+                start1++; start2++;
+            }
+            return ret;
+        }
+
+
+    }
+
+}
